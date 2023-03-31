ffa91d3008f7b607588b01d2ed3927f5f26998ed
Romish-64 <valotromain6417@gmail.com>
2023-03-31 10:55:55 +0200

    Step.8

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,60 @@
 import { useState } from 'react'
-import { useEffect } from 'react'
 import './App.css'
 import MyPokemonCard from './components/PokemonCard'
 import Navbar from './components/Navbar'
-import { func } from 'prop-types'
 function App() {
   const pokemonList = [
     {
       name: "Bulbasaur",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      type: "Grass",
     },
     {
       name: "Charmander",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+      type: "Fire",
+
     },
     {
       name: "Squirtle",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+      type: "Water",
     },
     {
       name: "Pikachu",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+      type: "Electric",
     },
     {
       name: "Mew",
     },
   ];
 
-  useEffect(
-    () => {
-      alert("Hello fellow pokemon trainer!")
-    },
-    []
-  )
-
   const [currentPokemonIndex, setCurrentPokemonIndex] = useState(0);
+  const [currentPokemonType, setCurrentPokemonType] = useState(0);
 
-  pokemonList[currentPokemonIndex].name === "Pikachu" ? alert("Pika, pikachuuuuu!!") : "";
 
   return (
     <div>
       <MyPokemonCard pokemon={pokemonList[currentPokemonIndex]} />
-      <Navbar currentPokemonIndex={currentPokemonIndex} setCurrentPokemonIndex={setCurrentPokemonIndex} pokemonList={pokemonList} />
+
+      <div className="boutons">
+        {pokemonList.map((pokemon, index) => (
+          <Navbar
+            key={pokemon.name}
+            name={pokemon.name}
+            index={index}
+            type={pokemon.type}
+            currentPokemonIndex={currentPokemonIndex}
+            setCurrentPokemonIndex={setCurrentPokemonIndex}
+            setCurrentPokemonType={setCurrentPokemonType}
+            currentPokemonType={currentPokemonType} />
+        ))}
+      </div>
     </div>
   )
 }
